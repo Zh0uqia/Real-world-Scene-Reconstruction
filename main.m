d5ea29849bb8b9a4330fc271a31e6465a1897c10
周倩颖 <zhouqianying@zhouqianyingdeMacBook-Air.local>
2019-09-03 23:20:03 -0400

    modification

--- main.m
+++ main.m
@@ -1,10 +1,3 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Penn State University
-%% EE554 Computer Vision II (2019 Spring) Project 1
-%% Rui Yu & Qianying Zhou
-%% 04/09/2019
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 clc
 clear all
 close all
